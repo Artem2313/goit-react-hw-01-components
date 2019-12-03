14faf5d2d9bbc27e6ae3ee23d5001cc3ed7b277e
artem2313 <huselnykov@gmail.com>
2019-12-03 22:30:58 +0200

    first_task_done_but_needs_css

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-// import ProductList from './ProductList';
-// import Panel from './Panel';
-import Dropdown from './Dropdown/Dropdown';
-// import Button from './Button/Button';
-// import products from '../products.json';
-const status = true;
+import profile from './profile/profile.json';
+import Profile from './profile/Profile';
 
 const App = () => (
-  // <
-  // div > {
-  //   /* <Button
-  //         label="Click me!"
-  //         icon="https://avatars2.githubusercontent.com/u/39895671?s=400&v=4"
-  //         // disabled
-  //       /> */
-  // }
-
-  <Dropdown isOpen={status} />
-
-  // {
-  //   /* <Panel title="Top Products">
-  //         <ProductList items={products} />
-  //       </Panel> */
-  // } <
-  // /div>
+  <>
+    <Profile users={profile} />
+  </>
 );
 
 export default App;
---
+++ src/components/profile/ProfileDetails.js
@@ -0,0 +1,40 @@
+/* eslint-disable react/forbid-prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './Profile.module.css';
+
+const ProfileDetails = ({ name, tag, location, avatar, stats }) => (
+  <div className={classes.profile}>
+    <div className={classes.description}>
+      <img src={avatar} alt="user avatar" className="avatar" />
+      <p className="name">{name}</p>
+      <p className="tag">{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+
+    <ul className={classes.stats}>
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+ProfileDetails.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
+
+export default ProfileDetails;
