7a614615ac5ac21e3b11f4b27f55619b1a0942ed
artem2313 <huselnykov@gmail.com>
2019-12-03 15:36:40 +0200

    Updates

---
+++ precache-manifest.9b244cffad892db7d75ed35ccb6d94fe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cf8dae847229e5965df053cd6b1aa5",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "ee08cb0114b800fcebc2",
+    "url": "/goit-react-hw-01-components/static/js/2.0e2c92b0.chunk.js"
+  },
+  {
+    "revision": "02d830a54c00e081ca26",
+    "url": "/goit-react-hw-01-components/static/js/main.3918ac4b.chunk.js"
+  },
+  {
+    "revision": "c4036953cb714bd39313",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.02e29828.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Artem2313/goit-react-hw-01-components/precache-manifest.3b157ed36f0446f51cbb4f3199566321.js"
+  "/goit-react-hw-01-components/precache-manifest.9b244cffad892db7d75ed35ccb6d94fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Artem2313/goit-react-hw-01-components/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
